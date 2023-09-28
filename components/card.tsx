9bbd27e00be12c5bed90ef624fb7bc532531d924
takehiro <iwasakitakehiro2@gmail.com>
2023-09-28 13:04:33 +0900

    検索修正

--- components/card.tsx
+++ components/card.tsx
@@ -21,75 +21,80 @@ interface Props {
 
 const CardComponent = ({ job }: { job: Props }) => {
   return (
-    <div className="w-4/5 mx-auto flex justify-center flex-wrap gap-10">
-      {job?.map(
-        (item: {
-          type: string;
-          id: number;
-          title: string;
-          name: string;
-          imageUrl?: string;
-          company: {
+    <>
+      <div className="text-center mb-10 text-4xl font-bold">
+        <p>求人一覧</p>
+      </div>
+      <div className="w-4/5 mx-auto flex justify-center flex-wrap gap-10">
+        {job?.map(
+          (item: {
+            type: string;
+            id: number;
+            title: string;
             name: string;
-          };
-          salary: string;
-          location: string;
-        }) => {
-          let imgSrc: string =
-            item.imageUrl ?? "/images/AdobeStock_101676859.jpeg";
-          return (
-            <div
-              key={item.id}
-              className="w-80 bg-white shadow-lg rounded-lg overflow-hidden fade-group"
-            >
-              <div className="flex justify-center items-center p-4">
-                <Image
-                  src={imgSrc}
-                  alt="メイン写真"
-                  className="h-48 object-cover rounded"
-                />
-              </div>
-              <div className="p-4 space-y-3">
-                {item.type === "FullTime" && (
-                  <p className="text-green-500 inline-block w-20 border border-green-500 text-center py-1 rounded text-xs">
-                    正社員
+            imageUrl?: string;
+            company: {
+              name: string;
+            };
+            salary: string;
+            location: string;
+          }) => {
+            let imgSrc: string =
+              item.imageUrl ?? "/images/AdobeStock_101676859.jpeg";
+            return (
+              <div
+                key={item.id}
+                className="w-80 bg-white shadow-lg rounded-lg overflow-hidden fade-group"
+              >
+                <div className="flex justify-center items-center p-4">
+                  <Image
+                    src={imgSrc}
+                    alt="メイン写真"
+                    className="h-48 object-cover rounded"
+                  />
+                </div>
+                <div className="p-4 space-y-3">
+                  {item.type === "FullTime" && (
+                    <p className="text-green-500 inline-block w-20 border border-green-500 text-center py-1 rounded text-xs">
+                      正社員
+                    </p>
+                  )}
+                  {item.type === "PartTime" && (
+                    <p className="text-orange-500 inline-block w-20 border border-orange-500 text-center py-1 rounded text-xs">
+                      アルバイト
+                    </p>
+                  )}
+                  {item.type === "Contract" && (
+                    <p className="text-blue-500 inline-block w-20 border border-blue-500 text-center py-1 rounded text-xs">
+                      派遣
+                    </p>
+                  )}
+                  <h2 className="text-lg font-semibold">{item.company.name}</h2>
+                  <p>
+                    {item.title.length > 30
+                      ? item.title.slice(0, 30) + "..."
+                      : item.title}
                   </p>
-                )}
-                {item.type === "PartTime" && (
-                  <p className="text-orange-500 inline-block w-20 border border-orange-500 text-center py-1 rounded text-xs">
-                    アルバイト
-                  </p>
-                )}
-                {item.type === "Contract" && (
-                  <p className="text-blue-500 inline-block w-20 border border-blue-500 text-center py-1 rounded text-xs">
-                    派遣
-                  </p>
-                )}
-                <h2 className="text-lg font-semibold">{item.company.name}</h2>
-                <p>
-                  {item.title.length > 30
-                    ? item.title.slice(0, 30) + "..."
-                    : item.title}
-                </p>
-                {item.type === "FullTime" && <p>年収 : {item.salary}万円~</p>}
-                {item.type === "PartTime" && <p>時給 : {item.salary}円~</p>}
-                {item.type === "Contract" && <p>時給 : {item.salary}円~</p>}
-                <p>勤務地 : {item.location}</p>
+                  {item.type === "FullTime" && <p>年収 : {item.salary}万円~</p>}
+                  {item.type === "PartTime" && <p>時給 : {item.salary}円~</p>}
+                  {item.type === "Contract" && <p>時給 : {item.salary}円~</p>}
+                  <p>勤務地 : {item.location}</p>
+                </div>
+                <div className="p-4 flex justify-center">
+                  <NextLink
+                    className="bg-green-500 hover:bg-green-600 text-white rounded px-4 py-2 w-full text-center"
+                    href={`/jobs/${item.id}?company=${item.company.name}&title=${item.title}`}
+                  >
+                    求人詳細
+                  </NextLink>
+                </div>
               </div>
-              <div className="p-4 flex justify-center">
-                <NextLink
-                  className="bg-green-500 hover:bg-green-600 text-white rounded px-4 py-2 w-full text-center"
-                  href={`/jobs/${item.id}?company=${item.company.name}&title=${item.title}`}
-                >
-                  求人詳細
-                </NextLink>
-              </div>
-            </div>
-          );
-        }
-      )}
-      {job?.length === 0 && <div>該当する求人はありません</div>}
-    </div>
+            );
+          }
+        )}
+        {job?.length === 0 && <div>該当する求人はありません</div>}
+      </div>
+    </>
   );
 };
 
--- pages/discover/index.js
+++ pages/discover/index.js
@@ -88,67 +88,70 @@ const List = ({ data }) => {
           <Box
             display="flex"
             justifyContent="center"
-            gap="2.5rem"
+            gap="2rem"
             flexWrap="wrap"
           >
-            <Box mb={4} w="40">
-              <FormLabel htmlFor="employment-type">雇用形態</FormLabel>
-              <Select
-                id="employment-type"
-                name="type"
-                value={formData.type}
-                onChange={handleChange}
-              >
-                <option value="" disabled>
-                  雇用形態
-                </option>
-                <option value="FullTime">正社員</option>
-                <option value="PartTime">アルバイト</option>
-                <option value="Contract">派遣</option>
-              </Select>
-            </Box>
-            <Box mb={4} w="40">
-              <FormLabel htmlFor="region">地域</FormLabel>
-              <Select
-                id="region"
-                name="region"
-                value={formData.region}
-                onChange={handleChange}
-              >
-                <option value="" disabled>
-                  地域
-                </option>
-                <option value="Ichihara">市原</option>
-                <option value="Chiharadai">ちはら台</option>
-                <option value="Goi">五井</option>
-                <option value="Tatsumidai">辰巳台</option>
-                <option value="Kokubunjidai">国分寺台</option>
-                <option value="Anesaki">姉崎</option>
-                <option value="Shizu">市津</option>
-                <option value="Sanwa">三和</option>
-                <option value="Yusyu">有秋</option>
-                <option value="Nansou">南総</option>
-                <option value="Kamo">加茂</option>
-              </Select>
-            </Box>
-            <Box mb={4} w="40">
-              <FormLabel htmlFor="job-type">職種</FormLabel>
-              <Select
-                id="job-type"
-                name="industry"
-                value={formData.industry}
-                onChange={handleChange}
-              >
-                <option value="" disabled>
-                  職種
-                </option>
-                <option value="Service">サービス業</option>
-                <option value="Construction">建設業</option>
-                <option value="hairSalon">美容室</option>
-                <option value="Restaurant">飲食業</option>
-                <option value="Childcare">保育士</option>
-              </Select>
-            </Box>
+            <div>
+              <Box w="40">
+                <Select
+                  id="employment-type"
+                  name="type"
+                  value={formData.type}
+                  onChange={handleChange}
+                >
+                  <option value="" disabled>
+                    雇用形態
+                  </option>
+                  <option value="FullTime">正社員</option>
+                  <option value="PartTime">アルバイト</option>
+                  <option value="Contract">派遣</option>
+                </Select>
+              </Box>
+            </div>
+            <div>
+              <Box w="40">
+                <Select
+                  id="region"
+                  name="region"
+                  value={formData.region}
+                  onChange={handleChange}
+                >
+                  <option value="" disabled>
+                    地域
+                  </option>
+                  <option value="Ichihara">市原</option>
+                  <option value="Chiharadai">ちはら台</option>
+                  <option value="Goi">五井</option>
+                  <option value="Tatsumidai">辰巳台</option>
+                  <option value="Kokubunjidai">国分寺台</option>
+                  <option value="Anesaki">姉崎</option>
+                  <option value="Shizu">市津</option>
+                  <option value="Sanwa">三和</option>
+                  <option value="Yusyu">有秋</option>
+                  <option value="Nansou">南総</option>
+                  <option value="Kamo">加茂</option>
+                </Select>
+              </Box>
+            </div>
+            <div>
+              <Box w="40">
+                <Select
+                  id="job-type"
+                  name="industry"
+                  value={formData.industry}
+                  onChange={handleChange}
+                >
+                  <option value="" disabled>
+                    職種
+                  </option>
+                  <option value="Service">サービス業</option>
+                  <option value="Construction">建設業</option>
+                  <option value="hairSalon">美容室</option>
+                  <option value="Restaurant">飲食業</option>
+                  <option value="Childcare">保育士</option>
+                </Select>
+              </Box>
+            </div>
           </Box>
           <Box textAlign="center" mt={10}>
             <Button
